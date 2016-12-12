d2503a3e1e722b45c93480294fc93dcbf721e760
Scott Aiton <scott@aiton.net>
2016-12-11 22:01:20 -0700

    fixed roundoff error in TriDiagSolver

--- domain_decomp/1D-example/TriDiagSolver.cpp
+++ domain_decomp/1D-example/TriDiagSolver.cpp
@@ -19,27 +19,31 @@ void TriDiagSolver::solve(Domain &dom)
 	const int    last_i = dom.size() - 1;
 	const double h      = dom.spaceDelta();
 
-	double left_boundary = left_bnd;
+	double left_boundary  = 2 * left_bnd;
+	double top_left_coeff = -3.0;
 	if (dom.hasLeftNbr()) {
-		left_boundary = (dom.leftNbr().uCurr().back() + u.at(0)) / 2;
+		top_left_coeff = -2.0;
+		left_boundary  = dom.leftNbr().uCurr().back();
 	}
-	double right_boundary = right_bnd;
+	double right_boundary     = 2 * right_bnd;
+	double bottom_right_coeff = -3.0;
 	if (dom.hasRightNbr()) {
-		right_boundary = (u.at(last_i) + dom.rightNbr().uPrev().front()) / 2;
+		bottom_right_coeff = -2.0;
+		right_boundary     = dom.rightNbr().uPrev().front();
 	}
 
 	// first value
-	c_prime[0] = 1.0 / -3.0;
-	d_prime[0] = (uxx.at(0) * h * h - 2 * left_boundary) / (-3.0);
+	c_prime[0] = 1.0 / top_left_coeff;
+	d_prime[0] = (uxx.at(0) * h * h - left_boundary) / top_left_coeff;
 	// in between values
 	for (int i = 1; i < last_i; i++) {
 		c_prime[i] = 1.0 / (-2.0 - c_prime[i - 1]);
 		d_prime[i] = (uxx.at(i) * h * h - d_prime[i - 1]) / (-2.0 - c_prime[i - 1]);
 	}
 	// last value
-	c_prime[last_i] = 1.0 / (-3.0 - c_prime[last_i - 1]);
-	d_prime[last_i] = (uxx.at(last_i) * h * h - 2 * right_boundary - d_prime[last_i - 1])
-	                  / (-3.0 - c_prime[last_i - 1]);
+	c_prime[last_i] = 1.0 / (bottom_right_coeff - c_prime[last_i - 1]);
+	d_prime[last_i] = (uxx.at(last_i) * h * h - right_boundary - d_prime[last_i - 1])
+	                  / (bottom_right_coeff - c_prime[last_i - 1]);
 
 	// back substitute
 	u.at(last_i) = d_prime[last_i];
