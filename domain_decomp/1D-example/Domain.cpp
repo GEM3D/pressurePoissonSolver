94b894bb5d5a8d38c2767ba322908b74c8acd46f
Scott Aiton <scott@aiton.net>
2016-12-11 22:43:04 -0700

    discovered that valarray exists, swapped all uses of vector with valarray

--- domain_decomp/1D-example/Domain.cpp
+++ domain_decomp/1D-example/Domain.cpp
@@ -5,23 +5,17 @@ using namespace std;
 Domain::Domain(double d_begin, double d_end, int m, double f(double x))
 {
 	this->m      = m;
-	u_curr_ptr            = new vector<double>(m);
-	u_prev_ptr            = new vector<double>(m);
-	u_xx          = vector<double>(m);
+	u_curr       = valarray<double>(m);
+	u_prev       = valarray<double>(m);
+	u_xx         = valarray<double>(m);
 	domain_begin = d_begin;
 	domain_end   = d_end;
 	h            = (d_end - d_begin) / m;
 
 	for (int i = 0; i < m; i++) {
-		double x  = domain_begin + (i + 0.5) / m * (domain_end - domain_begin);
-		u_xx.at(i) = f(x);
+		double x = domain_begin + (i + 0.5) / m * (domain_end - domain_begin);
+		u_xx[i]  = f(x);
 	}
-}
-
-Domain::~Domain()
-{
-	delete u_curr_ptr;
-	delete u_prev_ptr;
 }
 
 double Domain::spaceDelta() { return h; }
@@ -32,23 +26,6 @@ bool   Domain::hasLeftNbr() { return left_nbr_ptr != nullptr; }
 bool   Domain::hasRightNbr() { return right_nbr_ptr != nullptr; }
 void Domain::setLeftNbr(Domain &nbr) { left_nbr_ptr = &nbr; }
 void Domain::setRightNbr(Domain &nbr) { right_nbr_ptr = &nbr; }
-vector<double> &                 Domain::uxx() { return u_xx; }
-vector<double> &                 Domain::uCurr() { return *u_curr_ptr; }
-vector<double> &                 Domain::uPrev() { return *u_prev_ptr; }
 Domain &                         Domain::leftNbr() { return *left_nbr_ptr; }
 Domain &                         Domain::rightNbr() { return *right_nbr_ptr; }
-void                             Domain::swapCurrPrev()
-{
-	vector<double> *tmp = u_curr_ptr;
-	u_curr_ptr          = u_prev_ptr;
-	u_prev_ptr          = tmp;
-}
-double Domain::l2norm()
-{
-	double retval = 0;
-	for (int i = 0; i < m; i++) {
-		double diff = u_curr_ptr->at(i) - u_prev_ptr->at(i);
-		retval += diff * diff;
-	}
-	return sqrt(retval);
-}
+void                             Domain::swapCurrPrev() { u_curr.swap(u_prev); }
--- domain_decomp/1D-example/TriDiagSolver.cpp
+++ domain_decomp/1D-example/TriDiagSolver.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+#include <valarray>
 using namespace std;
 #include "TriDiagSolver.h"
 
@@ -10,44 +10,49 @@ TriDiagSolver::TriDiagSolver(double left_boundary, double right_boundary)
 
 void TriDiagSolver::solve(Domain &dom)
 {
-	vector<double> &uxx = dom.uxx();
-	vector<double> &u   = dom.uCurr();
-	vector<double>  d_prime(dom.size());
-	vector<double>  c_prime(dom.size());
+	valarray<double> &u_xx = dom.u_xx;
+	valarray<double> &u    = dom.u_curr;
+	valarray<double>  d_prime(dom.size());
+	valarray<double>  c_prime(dom.size());
 
 	// get some variables that are used for convenience
 	const int    last_i = dom.size() - 1;
 	const double h      = dom.spaceDelta();
 
+	// determine left boundary conditions
 	double left_boundary  = 2 * left_bnd;
 	double top_left_coeff = -3.0;
 	if (dom.hasLeftNbr()) {
-		top_left_coeff = -2.0;
-		left_boundary  = dom.leftNbr().uCurr().back();
+		top_left_coeff             = -2.0;
+		valarray<double> &left_sol = dom.leftNbr().u_curr;
+		left_boundary              = left_sol[left_sol.size() - 1];
 	}
+
+	// determine right boundary conditions
 	double right_boundary     = 2 * right_bnd;
 	double bottom_right_coeff = -3.0;
 	if (dom.hasRightNbr()) {
-		bottom_right_coeff = -2.0;
-		right_boundary     = dom.rightNbr().uPrev().front();
+		bottom_right_coeff          = -2.0;
+		valarray<double> &right_sol = dom.rightNbr().u_prev;
+		right_boundary              = right_sol[0];
 	}
 
 	// first value
 	c_prime[0] = 1.0 / top_left_coeff;
-	d_prime[0] = (uxx.at(0) * h * h - left_boundary) / top_left_coeff;
+	d_prime[0] = (u_xx[0] * h * h - left_boundary) / top_left_coeff;
 	// in between values
 	for (int i = 1; i < last_i; i++) {
 		c_prime[i] = 1.0 / (-2.0 - c_prime[i - 1]);
-		d_prime[i] = (uxx.at(i) * h * h - d_prime[i - 1]) / (-2.0 - c_prime[i - 1]);
+		d_prime[i] = (u_xx[i] * h * h - d_prime[i - 1]) / (-2.0 - c_prime[i - 1]);
 	}
 	// last value
 	c_prime[last_i] = 1.0 / (bottom_right_coeff - c_prime[last_i - 1]);
-	d_prime[last_i] = (uxx.at(last_i) * h * h - right_boundary - d_prime[last_i - 1])
+	d_prime[last_i] = (u_xx[last_i] * h * h - right_boundary - d_prime[last_i - 1])
 	                  / (bottom_right_coeff - c_prime[last_i - 1]);
 
 	// back substitute
-	u.at(last_i) = d_prime[last_i];
+	u[last_i] = d_prime[last_i];
 	for (int i = last_i - 1; i >= 0; i--) {
-		u.at(i) = d_prime[i] - c_prime[i] * u.at(i + 1);
+		u[i] = d_prime[i] - c_prime[i] * u[i + 1];
 	}
 }
