7a35a786535e5c159bf17dfe0b93024944deb787
Scott Aiton <scott@aiton.net>
2016-12-16 13:56:21 -0700

    got the 1d example working with the eigen libraryw

--- domain_decomp/1D-example/heat.cpp
+++ domain_decomp/1D-example/heat.cpp
@@ -1,3 +1,5 @@
+#include <Eigen/Dense>
+#include <Eigen/LU>
 #include <cassert>
 #include <cmath>
 #include <fstream>
@@ -9,6 +11,7 @@
 #define PI M_PI
 
 using namespace std;
+using namespace Eigen;
 #include "Domain.h"
 #include "TriDiagSolver.h"
 
@@ -18,10 +21,14 @@ extern "C" void dgesv_(int *n, int *nrhs, double *a, int *lda, int *ipiv, double
 extern "C" void dgecon_(char *norm, int *n, double *A, int *lda, double *Anorm, double *rcond,
                         double *work, int *iwork, int *info);
 
-//double uxx_init(double x) { return -PI * PI * sin(PI * x); }
-//double exact_solution(double x) { return sin(PI * x); }
-double uxx_init(double x) { return -0.5 + abs(x-0.5); }
-double exact_solution(double x) { return 1.0/24.0 - (x-0.5)*(x-0.5)/4.0 + abs(x-0.5)*abs(x-0.5)*abs(x-0.5)/6.0; }
+// double uxx_init(double x) { return -PI * PI * sin(PI * x); }
+// double exact_solution(double x) { return sin(PI * x); }
+double uxx_init(double x) { return -0.5 + abs(x - 0.5); }
+double exact_solution(double x)
+{
+	return 1.0 / 24.0 - (x - 0.5) * (x - 0.5) / 4.0
+	       + abs(x - 0.5) * abs(x - 0.5) * abs(x - 0.5) / 6.0;
+}
 double error(vector<Domain> &dmns)
 {
 	double l2norm     = 0;
@@ -49,7 +56,7 @@ double error(vector<Domain> &dmns)
  *
  * @return the difference between the gamma values and the computed value at the domain
  */
-valarray<double> solveOnAllDomains(TriDiagSolver &tds, vector<Domain> &dmns)
+VectorXd solveOnAllDomains(TriDiagSolver &tds, vector<Domain> &dmns)
 {
 	// solve over the domains
 	for (Domain &d : dmns) {
@@ -57,8 +64,8 @@ valarray<double> solveOnAllDomains(TriDiagSolver &tds, vector<Domain> &dmns)
 	}
 
 	// get the difference between the gamma value and computed solution at the interface
-	valarray<double> z(dmns.size() - 1);
-	for (size_t i = 0; i < z.size(); i++) {
+	VectorXd z(dmns.size() - 1);
+	for (int i = 0; i < z.size(); i++) {
 		Domain &left_dmn  = dmns[i];
 		Domain &right_dmn = dmns[i + 1];
 		double  gamma     = left_dmn.rightGamma();
@@ -138,7 +145,7 @@ int main(int argc, char *argv[])
 	}
 
 	// create an array to store the gamma values for each of the interfaces
-	valarray<double> gammas(num_domains - 1);
+	VectorXd gammas(num_domains - 1);
 
 	// set the gamma pointers
 	if (num_domains > 1) {
@@ -154,77 +161,38 @@ int main(int argc, char *argv[])
 	double condition_number;
 	if (num_domains > 1) {
 		// solve with gammas set to zero
-		gammas             = 0;
-		valarray<double> b = solveOnAllDomains(tds, dmns);
+		VectorXd b = solveOnAllDomains(tds, dmns);
 
 		if (print_matrix) {
 			cout << "b value(s):\n";
-			for (double x : b) {
-				cout << x << ' ';
-			}
+			cout << b;
 			cout << "\n\n";
 		}
 
 		// build the A matrix
-		int              n = gammas.size();
-		valarray<double> A(n * n);
+		int      n = gammas.size();
+		MatrixXd A(n, n);
 		for (int i = 0; i < n; i++) {
 			gammas[i] = 1.0;
-			A[slice(i * n, n, 1)] = solveOnAllDomains(tds, dmns) - b;
+			A.col(i)  = solveOnAllDomains(tds, dmns) - b;
 			gammas[i] = 0.0;
 		}
 
 		if (print_matrix) {
-			cout << "A matrix:\n";
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					cout << A[n * i + j] << '\t';
-				}
-				cout << '\n';
-			}
+			cout << "The Schur complement matrix:\n";
+			cout << A;
 			cout << "\n\n";
 		}
 
-		// get the condition number of A
-		double rcond;
-
-		{
-			// I'm putting these in their own scope so I don't clutter up the namespace with these
-			// variables
-			char             norm = '1';
-			valarray<double> col_sum(n);
-			for (int i = 0; i < n; i++) {
-				valarray<double> col = A[slice(i * n, n, 1)];
-				col_sum[i]           = abs(col).sum();
-			}
-			double           A_norm = col_sum.max();
-			valarray<double> work(4 * n);
-			valarray<int>    iwork(n);
-			int              info;
-			dgecon_(&norm, &n, &A[0], &n, &A_norm, &rcond, &work[0], &iwork[0], &info);
-			assert(info == 0);
-		}
-		condition_number = 1.0 / rcond;
-
 		// solve for the gamma values
-		{
-			int           one = 1;
-			valarray<int> ipiv(n);
-			int           info;
-			dgesv_(&n, &one, &A[0], &n, &ipiv[0], &b[0], &n, &info);
-			assert(info == 0);
-		}
-
-		// the solution gets stored in the b array
-		for (int i = 0; i < n; i++) {
-			gammas[i] = -b[i];
-		}
+		FullPivLU<MatrixXd> lu(A);
+		VectorXd            tmp = lu.solve(b);
+		gammas                  = -tmp;
+		condition_number        = 1.0 / lu.rcond();
 
 		if (print_matrix) {
 			cout << "calculated gamma value(s):\n";
-			for (double x : gammas) {
-				cout << x << ' ';
-			}
+			cout << gammas;
 			cout << "\n\n";
 		}
 	}
