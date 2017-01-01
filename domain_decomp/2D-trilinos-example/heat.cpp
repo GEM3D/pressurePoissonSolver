10f9bacd496302b814314ae34f03093821136e86
Scott Aiton <scott@aiton.net>
2017-01-01 12:57:35 -0700

    got single-domain 2D example working with trilinos

--- domain_decomp/2D-trilinos-example/heat.cpp
+++ domain_decomp/2D-trilinos-example/heat.cpp
@@ -17,7 +17,8 @@
 
 using namespace Galeri;
 
-Epetra_CrsMatrix *generate2DLaplacian(const Teuchos::RCP<Epetra_Map> Map, const int nx, const int ny, const double h_x, const double h_y);
+Epetra_CrsMatrix *generate2DLaplacian(const Teuchos::RCP<Epetra_Map> Map, const int nx,
+                                      const int ny, const double h_x, const double h_y);
 
 // the functions that we are using
 double ffun(double x, double y) { return -5 * M_PI * M_PI * sin(M_PI * x) * cos(2 * M_PI * y); }
@@ -38,8 +39,8 @@ int main(int argv, char *argc[])
 	Teuchos::ParameterList GaleriList;
 
 	// Set the number of discretization points in the x and y direction.
-    int nx = 5;
-    int ny = 10;
+	int    nx  = 100;
+	int    ny  = 100;
 	double h_x = 1.0 / nx;
 	double h_y = 1.0 / ny;
 	GaleriList.set("nx", nx);
@@ -48,14 +49,14 @@ int main(int argv, char *argc[])
 	GaleriList.set("ly", 1.0);
 
 	// Create the map and matrix using the parameter list for a 2D Laplacian.
-	RCP<Epetra_Map>       Map    = rcp(CreateMap("Cartesian2D", Comm, GaleriList));
+	RCP<Epetra_Map> Map = rcp(CreateMap("Cartesian2D", Comm, GaleriList));
 
-	RCP<Epetra_CrsMatrix> A = rcp(generate2DLaplacian(Map,nx,ny,h_x,h_y));
+	RCP<Epetra_CrsMatrix> A = rcp(generate2DLaplacian(Map, nx, ny, h_x, h_y));
 
-    // Generate RHS vector
-	RCP<Epetra_MultiVector> f = rcp(new Epetra_MultiVector(*Map,1));
-	RCP<Epetra_MultiVector> u = rcp(new Epetra_MultiVector(*Map,1));
-	RCP<Epetra_MultiVector> exact = rcp(new Epetra_MultiVector(*Map,1));
+	// Generate RHS vector
+	RCP<Epetra_MultiVector> f     = rcp(new Epetra_MultiVector(*Map, 1));
+	RCP<Epetra_MultiVector> u     = rcp(new Epetra_MultiVector(*Map, 1));
+	RCP<Epetra_MultiVector> exact = rcp(new Epetra_MultiVector(*Map, 1));
 	{
 		// Use local indices to access the entries of f_data.
 		const int localLength      = f->MyLength();
@@ -65,33 +66,70 @@ int main(int argv, char *argc[])
 		cout << MyGlobalElements << "\n";
 		for (int i = 0; i < NumMyElements; i++) {
 			int    global_i = MyGlobalElements[i];
-			int    index_x  = i % nx;
-			int    index_y  = (i - index_x) / nx;
+			int    index_x  = global_i % nx;
+			int    index_y  = (global_i - index_x) / nx;
 			double x        = h_x / 2.0 + 1.0 * index_x / nx;
 			double y        = h_y / 2.0 + 1.0 * index_y / ny;
-			(*f)[0][i]            = ffun(x, y);
-			(*exact)[0][i]        = gfun(x, y);
+			(*f)[0][i]      = ffun(x, y);
+			(*exact)[0][i]  = gfun(x, y);
+		}
+		// add boundaries to rhs
+		for (int i = 0; i < NumMyElements; i++) {
+			int    global_i = MyGlobalElements[i];
+			int    index_x  = global_i % nx;
+			int    index_y  = (global_i - index_x) / nx;
+			double x        = h_x / 2.0 + 1.0 * index_x / nx;
+			double y        = h_y / 2.0 + 1.0 * index_y / ny;
+			if (index_x == 0) {
+				(*f)[0][i] += -2.0 / (h_x * h_x) * gfun(0.0, y);
+			}
+			if (index_x == nx - 1) {
+				(*f)[0][i] += -2.0 / (h_x * h_x) * gfun(1.0, y);
+			}
+			if (index_y == 0) {
+				(*f)[0][i] += -2.0 / (h_y * h_y) * gfun(x, 0.0);
+			}
+			if (index_y == ny - 1) {
+				(*f)[0][i] += -2.0 / (h_y * h_y) * gfun(x, 1.0);
+			}
 		}
 	}
 
 	// Print out the map and matrices
-	Map->Print(std::cout);
-	A->Print(std::cout);
-	f->Print(std::cout);
+	// Map->Print(std::cout);
+	// A->Print(std::cout);
+	// f->Print(std::cout);
 	exact->Print(std::cout);
 
-	// Create solver interface to Superlu with Amesos2 factory method
+	// Create solver interface with Amesos2 factory method
 	RCP<Amesos2::Solver<Epetra_CrsMatrix, Epetra_MultiVector> > solver
 	= Amesos2::create<Epetra_CrsMatrix, Epetra_MultiVector>("KLU2", A, u, f);
-    solver->solve();
+	solver->solve();
 
-    u->Print(std::cout);
+	double exact_norm;
+	exact->Norm2(&exact_norm);
+	{
+		// Use local indices to access the entries of f_data.
+		const int localLength      = f->MyLength();
+		int       NumMyElements    = Map->NumMyElements();
+		int *     MyGlobalElements = 0;
+		Map->MyGlobalElementsPtr(MyGlobalElements);
+		cout << MyGlobalElements << "\n";
+		for (int i = 0; i < NumMyElements; i++) {
+			(*exact)[0][i] -= (*u)[0][i];
+		}
+	}
+	u->Print(std::cout);
+	double diff_norm;
+	exact->Norm2(&diff_norm);
+	cout << diff_norm / exact_norm << "\n";
 
 	MPI_Finalize();
 	return 0;
 }
 
-Epetra_CrsMatrix *generate2DLaplacian(const Teuchos::RCP<Epetra_Map> Map, const int nx, const int ny, const double h_x, const double h_y)
+Epetra_CrsMatrix *generate2DLaplacian(const Teuchos::RCP<Epetra_Map> Map, const int nx,
+                                      const int ny, const double h_x, const double h_y)
 {
 	using Teuchos::RCP;
 	Epetra_CrsMatrix *Matrix = new Epetra_CrsMatrix(Copy, *Map, 5);
@@ -110,57 +148,57 @@ Epetra_CrsMatrix *generate2DLaplacian(const Teuchos::RCP<Epetra_Map> Map, const 
 	for (int i = 0; i < NumMyElements; ++i) {
 		int NumEntries = 0;
 		// determine index of of neighbors in row
+		int global_i = MyGlobalElements[i];
 		int ix, iy;
-		ix = i % nx;
-		iy = (i - ix) / nx;
+		ix = global_i % nx;
+		iy = (global_i - ix) / nx;
 
 		if (ix == 0)
 			left = -1;
 		else
-			left = i - 1;
+			left = global_i - 1;
 		if (ix == nx - 1)
 			right = -1;
 		else
-			right = i + 1;
+			right = global_i + 1;
 		if (iy == 0)
 			lower = -1;
 		else
-			lower = i - nx;
+			lower = global_i - nx;
 		if (iy == ny - 1)
 			upper = -1;
 		else
-			upper = i + nx;
+			upper = global_i + nx;
 
-
-		double diag = -2.0/(h_y*h_y)-2.0/(h_x*h_x);
+		double diag = -2.0 / (h_y * h_y) - 2.0 / (h_x * h_x);
 
 		if (left != -1) {
 			Indices[NumEntries] = left;
-			Values[NumEntries]  = 1.0/(h_y*h_y);
+			Values[NumEntries]  = 1.0 / (h_y * h_y);
 			++NumEntries;
 		} else {
-			diag -= 1.0/(h_y*h_y);
+			diag -= 1.0 / (h_y * h_y);
 		}
 		if (right != -1) {
 			Indices[NumEntries] = right;
-			Values[NumEntries]  = 1.0/(h_y*h_y);
+			Values[NumEntries]  = 1.0 / (h_y * h_y);
 			++NumEntries;
 		} else {
-			diag -= 1.0/(h_y*h_y);
+			diag -= 1.0 / (h_y * h_y);
 		}
 		if (lower != -1) {
 			Indices[NumEntries] = lower;
-			Values[NumEntries]  = 1.0/(h_x*h_x);
+			Values[NumEntries]  = 1.0 / (h_x * h_x);
 			++NumEntries;
 		} else {
-			diag -= 1.0/(h_x*h_x);
+			diag -= 1.0 / (h_x * h_x);
 		}
 		if (upper != -1) {
 			Indices[NumEntries] = upper;
-			Values[NumEntries]  = 1.0/(h_x*h_x);
+			Values[NumEntries]  = 1.0 / (h_x * h_x);
 			++NumEntries;
 		} else {
-			diag -= 1.0/(h_x*h_x);
+			diag -= 1.0 / (h_x * h_x);
 		}
 		// put the off-diagonal entries
 		Matrix->InsertGlobalValues(MyGlobalElements[i], NumEntries, &Values[0], &Indices[0]);
