e12b8aabd709ff6c776caf0466922f2bb226bb7d
Scott Aiton <scott@aiton.net>
2017-01-25 08:43:19 -0700

    more optimizations

--- domain_decomp/2D-trilinos-example/Domain.h
+++ domain_decomp/2D-trilinos-example/Domain.h
@@ -3,14 +3,13 @@
 #include "MyTypeDefs.h"
 #include <Epetra_Import.h>
 #include <Teuchos_RCP.hpp>
-#include <valarray>
 #include <cmath>
 #include <fftw3.h>
+#include <valarray>
 class Domain
 {
 	public:
 	std::valarray<double>  f;
-	std::valarray<double>  f_copy;
 	std::valarray<double>  u;
 	std::valarray<double>  tmp;
 	std::valarray<double>  denom;
@@ -18,18 +17,27 @@ class Domain
 	int                    ny;
 	double                 h_x;
 	double                 h_y;
-	std::valarray<double>   boundary_north;
-	std::valarray<double>   boundary_south;
-	std::valarray<double>   boundary_east;
-	std::valarray<double>   boundary_west;
-	bool                   has_north      = false;
-	bool                   has_south      = false;
-	bool                   has_east       = false;
-	bool                   has_west       = false;
+	std::valarray<double>  boundary_north;
+	std::valarray<double>  boundary_south;
+	std::valarray<double>  boundary_east;
+	std::valarray<double>  boundary_west;
+	std::valarray<double>  orig_north;
+	std::valarray<double>  orig_south;
+	std::valarray<double>  orig_east;
+	std::valarray<double>  orig_west;
+	std::slice             north;
+	std::slice             east;
+	std::slice             south;
+	std::slice             west;
+	bool                   has_north = false;
+	bool                   has_south = false;
+	bool                   has_east  = false;
+	bool                   has_west  = false;
 	Teuchos::RCP<map_type> domain_map;
+	fftw_plan              plan1;
+	fftw_plan              plan2;
 
-	Domain(std::valarray<double> &f, int nx, int ny, double h_x,
-	       double h_y)
+	Domain(std::valarray<double> &f, int nx, int ny, double h_x, double h_y)
 	{
 		this->f   = f;
 		this->nx  = nx;
@@ -37,34 +45,67 @@ class Domain
 		this->h_x = h_x;
 		this->h_y = h_y;
 
-		f_copy = f;
-        
-		u     = f;
-		tmp   = std::valarray<double>(nx * ny);
-		denom = std::valarray<double>(nx * ny);
-		for (int yi = 1; yi <= ny; yi++) {
-			for (int xi = 1; xi <= nx; xi++) {
-				denom[nx * (yi - 1) + xi - 1]
-				= -4 / (h_x * h_x) * std::pow(std::sin(xi * M_PI / (2 * nx)), 2)
-				  - 4 / (h_y * h_y) * std::pow(std::sin(yi * M_PI / (2 * ny)), 2);
-			}
+		std::valarray<double> f_copy = f;
+		tmp                          = std::valarray<double>(nx * ny);
+		u                            = std::valarray<double>(nx * ny);
+		denom                        = std::valarray<double>(nx * ny);
+		for (int xi = 1; xi <= nx; xi++) {
+			denom[std::slice((xi - 1) * ny, nx, 1)]
+			= -4 / (h_x * h_x) * std::pow(std::sin(xi * M_PI / (2 * nx)), 2);
 		}
+		std::valarray<double> ones(ny);
+		ones = 1;
+		for (int yi = 1; yi <= ny; yi++) {
+			denom[std::slice((yi - 1), ny, nx)]
+			-= 4 / (h_y * h_y) * std::pow(std::sin(yi * M_PI / (2 * ny)), 2) * ones;
+		}
+		// create fftw plans
+		plan1 = fftw_plan_r2r_2d(ny, nx, &f[0], &tmp[0], FFTW_RODFT10, FFTW_RODFT10, FFTW_MEASURE);
+
+		plan2 = fftw_plan_r2r_2d(ny, nx, &tmp[0], &u[0], FFTW_RODFT01, FFTW_RODFT01, FFTW_MEASURE);
+
+		f = f_copy;
+		// create slices
+		north = std::slice(nx * (ny - 1), nx, 1);
+		east  = std::slice((nx - 1), ny, nx);
+		south = std::slice(0, nx, 1);
+		west  = std::slice(0, ny, nx);
+
+		orig_north = std::valarray<double>(nx);
+		orig_north = f[north];
+		orig_east  = std::valarray<double>(ny);
+		orig_east  = f[east];
+		orig_south = std::valarray<double>(nx);
+		orig_south = f[south];
+		orig_west  = std::valarray<double>(ny);
+		orig_west  = f[west];
+	}
+
+	~Domain()
+	{
+		fftw_destroy_plan(plan1);
+		fftw_destroy_plan(plan2);
 	}
 
 	void solve()
 	{
-		f_copy = f;
-		if(has_north)f_copy[std::slice(nx*(ny-1),nx,1)] -= 2/(h_y*h_y)* boundary_north;
-		if(has_east)f_copy[std::slice((nx-1),ny,nx)] -=  2/(h_y*h_y)*boundary_east;
-		if(has_south)f_copy[std::slice(0,nx,1)] -= 2/(h_y*h_y)* boundary_south;
-		if(has_west)f_copy[std::slice(0,ny,nx)] -= 2/(h_y*h_y)* boundary_west;
-		
-		// create fftw plans
-		fftw_plan plan1 = fftw_plan_r2r_2d(ny, nx, &f_copy[0], &tmp[0], FFTW_RODFT10,
-		                                   FFTW_RODFT10, FFTW_MEASURE);
-
-		fftw_plan plan2
-		= fftw_plan_r2r_2d(ny, nx, &tmp[0], &u[0], FFTW_RODFT01, FFTW_RODFT01, FFTW_MEASURE);
+		// f_copy = f;
+		if (has_north) {
+			f[north] = orig_north;
+			f[north] -= 2 / (h_y * h_y) * boundary_north;
+		}
+		if (has_east) {
+			f[east] = orig_east;
+			f[east] -= 2 / (h_x * h_x) * boundary_east;
+		}
+		if (has_south) {
+			f[south] = orig_south;
+			f[south] -= 2 / (h_y * h_y) * boundary_south;
+		}
+		if (has_west) {
+			f[west] = orig_west;
+			f[west] -= 2 / (h_x * h_x) * boundary_west;
+		}
 
 		fftw_execute(plan1);
 
@@ -73,9 +114,6 @@ class Domain
 		fftw_execute(plan2);
 
 		u /= 4 * nx * ny;
-
-		fftw_destroy_plan(plan1);
-		fftw_destroy_plan(plan2);
 	}
 
 	void solveWithInterface(const vector_type &gamma, vector_type &diff)
@@ -86,19 +124,19 @@ class Domain
 		local_vector.Import(gamma, importer, Insert);
 		int curr_i = 0;
 		if (has_north) {
-			boundary_north = std::valarray<double>(&local_vector[0][curr_i],nx);
+			boundary_north = std::valarray<double>(&local_vector[0][curr_i], nx);
 			curr_i += nx;
 		}
 		if (has_east) {
-			boundary_east = std::valarray<double>(&local_vector[0][curr_i],ny);
+			boundary_east = std::valarray<double>(&local_vector[0][curr_i], ny);
 			curr_i += ny;
 		}
 		if (has_south) {
-			boundary_south = std::valarray<double>(&local_vector[0][curr_i],nx);
+			boundary_south = std::valarray<double>(&local_vector[0][curr_i], nx);
 			curr_i += nx;
 		}
 		if (has_west) {
-			boundary_west = std::valarray<double>(&local_vector[0][curr_i],ny);
+			boundary_west = std::valarray<double>(&local_vector[0][curr_i], ny);
 		}
 
 		// solve
