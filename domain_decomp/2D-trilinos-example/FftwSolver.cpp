9aebc5f262fe93c8c4ad448d2777e53cb76f72c3
Scott Aiton <scott@aiton.net>
2017-05-19 11:01:36 -0600

    fixed dirichlet case

--- domain_decomp/2D-trilinos-example/FftwSolver.cpp
+++ domain_decomp/2D-trilinos-example/FftwSolver.cpp
@@ -12,26 +12,29 @@ FftwSolver::FftwSolver(Domain *dom)
 	fftw_r2r_kind x_transform_inv = FFTW_RODFT01;
 	fftw_r2r_kind y_transform     = FFTW_RODFT10;
 	fftw_r2r_kind y_transform_inv = FFTW_RODFT01;
-	if (!d->neumann || (!d->hasNbr(Side::east) && !d->hasNbr(Side::west))) {
-		x_transform     = FFTW_REDFT10;
-		x_transform_inv = FFTW_REDFT01;
-	} else if (!d->hasNbr(Side::west)) {
-		x_transform     = FFTW_REDFT11;
-		x_transform_inv = FFTW_REDFT11;
-	} else if (!d->hasNbr(Side::east)) {
-		x_transform     = FFTW_RODFT11;
-		x_transform_inv = FFTW_RODFT11;
+	if (d->neumann) {
+		if (!d->hasNbr(Side::east) && !d->hasNbr(Side::west)) {
+			x_transform     = FFTW_REDFT10;
+			x_transform_inv = FFTW_REDFT01;
+		} else if (!d->hasNbr(Side::west)) {
+			x_transform     = FFTW_REDFT11;
+			x_transform_inv = FFTW_REDFT11;
+		} else if (!d->hasNbr(Side::east)) {
+			x_transform     = FFTW_RODFT11;
+			x_transform_inv = FFTW_RODFT11;
+		}
+		if (!d->hasNbr(Side::north) && !d->hasNbr(Side::south)) {
+			y_transform     = FFTW_REDFT10;
+			y_transform_inv = FFTW_REDFT01;
+		} else if (!d->hasNbr(Side::south)) {
+			y_transform     = FFTW_REDFT11;
+			y_transform_inv = FFTW_REDFT11;
+		} else if (!d->hasNbr(Side::north)) {
+			y_transform     = FFTW_RODFT11;
+			y_transform_inv = FFTW_RODFT11;
+		}
 	}
-	if (!d->neumann || (!d->hasNbr(Side::north) && !d->hasNbr(Side::south))) {
-		y_transform     = FFTW_REDFT10;
-		y_transform_inv = FFTW_REDFT01;
-	} else if (!d->hasNbr(Side::south)) {
-		y_transform     = FFTW_REDFT11;
-		y_transform_inv = FFTW_REDFT11;
-	} else if (!d->hasNbr(Side::north)) {
-		y_transform     = FFTW_RODFT11;
-		y_transform_inv = FFTW_RODFT11;
-	}
+
 	plan1
 	= fftw_plan_r2r_2d(d->n, d->n, &f_copy[0], &tmp[0], y_transform, x_transform, FFTW_MEASURE);
 
@@ -39,40 +42,54 @@ FftwSolver::FftwSolver(Domain *dom)
 	= fftw_plan_r2r_2d(d->n, d->n, &tmp[0], &u[0], y_transform_inv, x_transform_inv, FFTW_MEASURE);
 
 	// create denom vector
-	if (d->neumann && (!d->hasNbr(Side::north) && !d->hasNbr(Side::south))) {
-		for (int yi = 0; yi < d->n; yi++) {
-			denom[slice(yi * d->n, d->n, 1)]
-			= -4 / (d->h_x * d->h_x) * pow(sin(yi * M_PI / (2 * d->n)), 2);
-		}
-	} else if (d->neumann && (!d->hasNbr(Side::south) || !d->hasNbr(Side::north))) {
-		for (int yi = 0; yi < d->n; yi++) {
-			denom[slice(yi * d->n, d->n, 1)]
-			= -4 / (d->h_x * d->h_x) * pow(sin((yi + 0.5) * M_PI / (2 * d->n)), 2);
-		}
-	} else {
+	if (!d->neumann) {
 		for (int yi = 0; yi < d->n; yi++) {
 			denom[slice(yi * d->n, d->n, 1)]
 			= -4 / (d->h_x * d->h_x) * pow(sin((yi + 1) * M_PI / (2 * d->n)), 2);
+		}
+	} else {
+		if (!d->hasNbr(Side::north) && !d->hasNbr(Side::south)) {
+			for (int yi = 0; yi < d->n; yi++) {
+				denom[slice(yi * d->n, d->n, 1)]
+				= -4 / (d->h_x * d->h_x) * pow(sin(yi * M_PI / (2 * d->n)), 2);
+			}
+		} else if (!d->hasNbr(Side::south) || !d->hasNbr(Side::north)) {
+			for (int yi = 0; yi < d->n; yi++) {
+				denom[slice(yi * d->n, d->n, 1)]
+				= -4 / (d->h_x * d->h_x) * pow(sin((yi + 0.5) * M_PI / (2 * d->n)), 2);
+			}
+		} else {
+			for (int yi = 0; yi < d->n; yi++) {
+				denom[slice(yi * d->n, d->n, 1)]
+				= -4 / (d->h_x * d->h_x) * pow(sin((yi + 1) * M_PI / (2 * d->n)), 2);
+			}
 		}
 	}
 
 	valarray<double> ones(d->n);
 	ones = 1;
 
-	if (d->neumann && (!d->hasNbr(Side::east) && !d->hasNbr(Side::west))) {
-		for (int xi = 0; xi < d->n; xi++) {
-			denom[slice(xi, d->n, d->n)]
-			-= 4 / (d->h_y * d->h_y) * pow(sin(xi * M_PI / (2 * d->n)), 2) * ones;
-		}
-	} else if (d->neumann && (!d->hasNbr(Side::west) || !d->hasNbr(Side::east))) {
-		for (int xi = 0; xi < d->n; xi++) {
-			denom[slice(xi, d->n, d->n)]
-			-= 4 / (d->h_y * d->h_y) * pow(sin((xi + 0.5) * M_PI / (2 * d->n)), 2) * ones;
-		}
-	} else {
+	if (!d->neumann) {
 		for (int xi = 0; xi < d->n; xi++) {
 			denom[slice(xi, d->n, d->n)]
 			-= 4 / (d->h_y * d->h_y) * pow(sin((xi + 1) * M_PI / (2 * d->n)), 2) * ones;
+		}
+	} else {
+		if (!d->hasNbr(Side::east) && !d->hasNbr(Side::west)) {
+			for (int xi = 0; xi < d->n; xi++) {
+				denom[slice(xi, d->n, d->n)]
+				-= 4 / (d->h_y * d->h_y) * pow(sin(xi * M_PI / (2 * d->n)), 2) * ones;
+			}
+		} else if (!d->hasNbr(Side::west) || !d->hasNbr(Side::east)) {
+			for (int xi = 0; xi < d->n; xi++) {
+				denom[slice(xi, d->n, d->n)]
+				-= 4 / (d->h_y * d->h_y) * pow(sin((xi + 0.5) * M_PI / (2 * d->n)), 2) * ones;
+			}
+		} else {
+			for (int xi = 0; xi < d->n; xi++) {
+				denom[slice(xi, d->n, d->n)]
+				-= 4 / (d->h_y * d->h_y) * pow(sin((xi + 1) * M_PI / (2 * d->n)), 2) * ones;
+			}
 		}
 	}
 }
