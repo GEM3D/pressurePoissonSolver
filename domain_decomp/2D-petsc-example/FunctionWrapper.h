846998c0b123c4da1f9caa14672d480f0cc2bfab
Scott Aiton <scott@aiton.net>
2018-01-17 22:26:00 -0700

    add function wrapper for 2D petsc solver

--- domain_decomp/2D-petsc-example/FunctionWrapper.h
+++ domain_decomp/2D-petsc-example/FunctionWrapper.h
@@ -1,37 +1,36 @@
 #ifndef FUNCTIONWRAPPER_H
 #define FUNCTIONWRAPPER_H
 #include "SchurHelper.h"
-#include "MyTypeDefs.h"
-#include <BelosLinearProblem.hpp>
-#include <BelosTpetraAdapter.hpp>
-#include <Teuchos_RCP.hpp>
-class FuncWrap : public Tpetra::Operator<scalar_type>
+#include <iostream>
+class FuncWrap
 {
 	public:
-	Teuchos::RCP<vector_type> *b;
-	Teuchos::RCP<vector_type> *u;
-	Teuchos::RCP<vector_type> *f;
-	SchurHelper *              sh;
-	FuncWrap(Teuchos::RCP<vector_type> *b, Teuchos::RCP<vector_type> *u,
-	         Teuchos::RCP<vector_type> *f, SchurHelper *sh)
+	PW<Vec>           u;
+	PW<Vec>           f;
+	SchurHelper *     sh = nullptr;
+	DomainCollection *dc = nullptr;
+	FuncWrap(SchurHelper *sh, DomainCollection *dc)
 	{
-		this->b  = b;
-		this->u  = u;
-		this->f  = f;
+		u        = dc->getNewDomainVec();
+		f        = dc->getNewDomainVec();
 		this->sh = sh;
+		this->dc = dc;
 	}
-	void apply(const vector_type &x, vector_type &y, Teuchos::ETransp mode = Teuchos::NO_TRANS,
-	           scalar_type alpha = Teuchos::ScalarTraits<scalar_type>::one(),
-	           scalar_type beta  = Teuchos::ScalarTraits<scalar_type>::zero()) const
+	static int multiply(Mat A, Vec x, Vec y)
 	{
-		for (size_t i = 0; i < x.getNumVectors(); i++) {
-			auto sx = *x.getVector(i);
-			auto sy = *y.getVectorNonConst(i);
-			sh->solveWithInterface(**f, **u, sx, sy);
-			sy.update(1, **b, 1);
-		}
+		FuncWrap *w;
+		MatShellGetContext(A, &w);
+		w->sh->solveWithInterface(w->f, w->u, x, y);
+		return 0;
 	}
-	Teuchos::RCP<const map_type> getDomainMap() const { return (**b).getMap(); }
-	Teuchos::RCP<const map_type> getRangeMap() const { return (**b).getMap(); }
+	PW_explicit<Mat> getMatrix()
+	{
+		PW<Mat> A;
+		int     M = dc->num_global_interfaces * dc->n;
+		int     m = dc->ifaces.size() * dc->n;
+		MatCreateShell(MPI_COMM_WORLD, m, m, M, M, this, &A);
+		MatShellSetOperation(A, MATOP_MULT, (void (*)(void)) multiply);
+		return A;
+	}
 };
 #endif
