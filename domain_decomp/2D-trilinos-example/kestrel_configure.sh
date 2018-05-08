31671915eb8063e06b9a37d8b31bfb1cd599880a
Scott Aiton <scott@aiton.net>
2018-05-08 17:12:42 -0600

    did some re-organizing

--- domain_decomp/2D-trilinos-example/kestrel_configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-source load.sh
-rm -r CMakeCache.txt CMakeFiles/
-export TRILINOS_DIR=/cm/shared/apps/trilinos/gcc-4.8.1/openmpi-2.0.1/hdf5-1.18.17/2.10.1/
-cmake -DCMAKE_CXX_COMPILER=mpic++ \
--DFFTW_LIB=/cm/shared/apps/fftw/openmpi/gcc/64/3.3.3/lib/libfftw3.a \
--DCMAKE_C_COMPILER=mpicc  ./
-
--- domain_decomp/2D-trilinos-example/load.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-module load gcc/4.8.1
-module load trilinos/cuda65/gcc-4.8.1/openmpi-2.0.1/12.10.1
-module load slurm/2.6.7
-module load cmake/3.5.1
-module load openmpi/cuda65/gcc4.8.1/2.0.1
